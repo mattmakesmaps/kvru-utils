7e587280f12aaeadc5018375841a5172148a2bc9
Matthew Kenny <matthewkenny@gmail.com>
2023-11-22 10:41:18 -0800

    update source files dirname

--- mp3-convert/mp3-convert/mp3-convert.py
+++ mp3-convert/mp3-convert/mp3-convert.py
@@ -8,18 +8,6 @@ from datetime import datetime
 from ledger import Ledger
 
 if __name__ == "__main__":
-    logger = logging.getLogger("mp3-convert")
-    logger.setLevel(logging.INFO)
-    logpath = os.path.dirname(os.path.realpath(__file__)) + "/mp3-convert.log"
-    loggerFileHandler = logging.FileHandler(logpath)
-    loggerConsoleHandler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
-    loggerFileHandler.setFormatter(formatter)
-    loggerConsoleHandler.setFormatter(formatter)
-    logger.addHandler(loggerFileHandler)
-    logger.addHandler(loggerConsoleHandler)
-
-    checksum_csv_path = os.path.dirname(os.path.realpath(__file__)) + "/mp3-convert-checksums.log"
 
     parser = argparse.ArgumentParser(description="Convert audio files to mp3 format.")
     parser.add_argument("inputdir")
@@ -31,6 +19,14 @@ if __name__ == "__main__":
         default="192k",
         help="Bitrate of the output wav files",
     )
+
+    parser.add_argument(
+        "-l",
+        "--logdir",
+        type=str,
+        help="Specify dir for logfiles. Two are created, one containing checksums and another containing stdout messages. Defaults to parent dir of this script. ",
+    )
+
     parser.add_argument(
         "--flat", action="store_true", help="Do not preserve directory structure"
     )
@@ -41,11 +37,23 @@ if __name__ == "__main__":
 
     INPUT_DIR = pathlib.Path(args.inputdir)
     OUTPUT_DIR = pathlib.Path(args.outputdir)
+    LOGPATH = os.path.dirname(os.path.realpath(__file__)) + "/mp3-convert.log"
+    CHECKSUM_CSV_PATH = os.path.dirname(os.path.realpath(__file__)) + "/mp3-convert-checksums.log"
+
+    logger = logging.getLogger("mp3-convert")
+    logger.setLevel(logging.INFO)
+    loggerFileHandler = logging.FileHandler(LOGPATH)
+    loggerConsoleHandler = logging.StreamHandler()
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+    loggerFileHandler.setFormatter(formatter)
+    loggerConsoleHandler.setFormatter(formatter)
+    logger.addHandler(loggerFileHandler)
+    logger.addHandler(loggerConsoleHandler)
 
     logger.info("Run Beginning. Input: %s, Output: %s", INPUT_DIR, OUTPUT_DIR)
     starttime = datetime.now()
 
-    ledger = Ledger(INPUT_DIR, OUTPUT_DIR, checksum_csv_path)
+    ledger = Ledger(INPUT_DIR, OUTPUT_DIR, CHECKSUM_CSV_PATH)
     ledger.search_for_files()
 
     logger.info("Found %d files in %s", len(ledger.files), INPUT_DIR)
