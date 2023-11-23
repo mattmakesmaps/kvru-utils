74fc9e8d183e9708025993e6cdf1d619176509eb
Matthew Kenny <matthewkenny@gmail.com>
2023-11-22 16:47:33 -0800

    Test notes

--- mp3convert/tests/test_mp3convert.py
+++ mp3convert/tests/test_mp3convert.py
@@ -1,10 +1,37 @@
 # Package import pattern REF: https://docs.python-guide.org/writing/structure/#test-suite
+import os
+import pathlib
+import shutil
 from context import mp3convert
 from mp3convert import Ledger
 
 import unittest
 
-class TestLedger(unittest.TestCase):
+TEST_DATA_DEST = pathlib.Path(os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_data', 'dest')))
+TEST_DATA_SOURCE = pathlib.Path(os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_data', 'source')))
 
-    def testLedger(self):
-        self.assertEqual(1, 1)
+class TestLedger(unittest.TestCase):
+    def setUpClass(self):
+        os.mkdir(TEST_DATA_DEST)
+
+    def testConversion(self):
+        ledger = Ledger(TEST_DATA_SOURCE, TEST_DATA_DEST)
+        ledger.search_for_files()
+        ledger.convert_all("192k")
+    
+    """
+    MK TESTS:
+    - non music files are not copied.
+    - check that files from root and subdirs are copied correctly.
+    """
+
+    def tearDownClass(self):
+        print("Tearing down {}".format(TEST_DATA_DEST))
+        shutil.rmtree(TEST_DATA_DEST)
+
+"""
+MK FUTURE TEST:
+- When future functionality is added to separate files with and without metadata,
+  test that files are copied to correct destination. Will need to be implemented as
+  a separate test class since destination structure will be different.
+"""
